570f6513fa6af33dca17a674d7b8248a0941fdc2
grossa2 <grossa2@my.erau.edu>
2021-03-09 15:17:53 -0700

    cleaned up clutter in this branch

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lti_utils_build_linker.cmd.genlibs -l"ti/devices/cc13x2_cc26x2/driverlib/bin/ccs/driverlib.lib" -llibc.a
-
--- Debug/syscfg/ti_ble_config.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-syscfg/ti_ble_config.obj: syscfg/ti_ble_config.c
-
-syscfg/ti_ble_config.c: 
